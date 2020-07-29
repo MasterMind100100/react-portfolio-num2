60b892188878ab715e2eda14f6f601027ffd726d
unknown <xavrobxav@gmail.com>
2020-07-29 09:15:29 -0600

    Building Out the Flexbox Container for the Navigation Component

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -8,16 +8,19 @@ export default class NavigationComponent extends Component {
 
   render() {
     return (
-      <div>
-        <NavLink exact to="/">
-          Home
-        </NavLink>
+      <div className="nav-wrapper">
+        <div className="left-side">
+          <NavLink exact to="/">
+            Home
+          </NavLink>
 
-        <NavLink to="/about-me">About</NavLink>
-        <NavLink to="/contact">Contact</NavLink>
-        <NavLink to="/blog">Blog</NavLink>
+          <NavLink to="/about-me">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+          <NavLink to="/blog">Blog</NavLink>
+        </div>
 
         {false ? <button>Add Blog</button> : null}
+        <div className="right-side">ajsdkljfa;ksdf</div>
       </div>
     );
   }
