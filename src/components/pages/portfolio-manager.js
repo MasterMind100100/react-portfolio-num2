b9e75dd79794900453bc742c47cd45bbd10ff42d
unknown <xavrobxav@gmail.com>
2020-08-07 08:00:22 -0600

    Populating Parent Component Data in React

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -19,9 +19,10 @@ export default class PortfolioManager extends Component {
   }
 
   handleSuccessfulFormSubmission(portfolioItem) {
-    // TODO
-    // update the portfolioItems state
-    // and add the portfolioItem to the list
+    this.setState({
+      portfolioItems: [portfolioItem].concat(this.state.portfolioItems),
+    });
+    console.log("Portfolio Submition", portfolioItem);
   }
 
   handleFormSubmissionError(error) {
@@ -30,9 +31,12 @@ export default class PortfolioManager extends Component {
 
   getPortfolioItems() {
     axios
-      .get("https://xavierroberts.devcamp.space/portfolio/portfolio_items", {
-        withCredentials: true,
-      })
+      .get(
+        "https://xavierroberts.devcamp.space/portfolio/portfolio_items?order_by=created_at&direction=desc",
+        {
+          withCredentials: true,
+        }
+      )
       .then((response) => {
         this.setState({
           portfolioItems: [...response.data.portfolio_items],
