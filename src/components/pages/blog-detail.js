0c0c6fdf3641a4c8b2a798ce20e11bc83357c6ee
unknown <xavrobxav@gmail.com>
2020-08-11 11:32:44 -0600

    Building the Initial Blog Detail Component

---
+++ src/components/pages/blog-detail.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class BlogDetail extends Component {
+  render() {
+    return (
+      <div>
+        <h1>blog detail</h1>
+        <h1>blog detail</h1>
+      </div>
+    );
+  }
+}
