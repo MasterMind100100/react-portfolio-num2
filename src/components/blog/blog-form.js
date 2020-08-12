f8ed08a36ef5e175a710d410e16f97f28d4ec22a
unknown <xavrobxav@gmail.com>
2020-08-11 19:32:51 -0600

    Creating the Initial Blog Form Component

---
+++ src/components/blog/blog-form.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+export default class extends Component {
+  render() {
+    return (
+      <form>
+        <input type="text" />
+        <input type="text" />
+
+        <button>Save</button>
+      </form>
+    );
+  }
+}
